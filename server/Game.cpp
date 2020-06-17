64cb81209ce0627a9de5a0b6c4f60eb563b306e7
Emiliano Gallardo <eagallardo@fi.uba.ar>
2020-06-17 20:36:12 -0300

    Agrego clase Game maneja eventos

--- server/Game.cpp
+++ server/Game.cpp
@@ -1,18 +1,34 @@
+#include <iostream>
 #include "Game.h"
 
 Game::Game() {
   this->idCounter = 1;
-  this->running = true;
+
+  this->addPlayer("Emi");
+  this->addCreature();
 }
 
 Game::~Game() {}
 
-bool Game::isRunning() const {
-  return this->running;
-}
+void Game::handle(Event& event) {
+  auto* player = (Player*) world.getEntity(event.getSenderId());
 
-void Game::stop() {
-  this->running = false;
+  switch(event.getAction()) {
+    case MOVE_UP:
+      player->moveUp();
+      break;
+    case MOVE_DOWN:
+      player->moveDown();
+      break;
+    case MOVE_RIGHT:
+      player->moveRight();
+      break;
+    case MOVE_LEFT:
+      player->moveLeft();
+      break;
+    default:
+      std::cout << "Opción invalida";
+  }
 }
 
 std::string Game::draw() {
@@ -20,31 +36,18 @@ std::string Game::draw() {
 }
 
 void Game::addCreature() {
-  Creature* creature = new Creature(world, idCounter);
+  auto* creature = new Creature(world, idCounter);
   creature->setPosition(5, 1);
   world.notify(NEW_ENTITY, creature);
   idCounter++;
 }
 
-void Game::addCharacter(std::string &name) {
-  Player* player = new Player(world, idCounter, name);
+void Game::addPlayer(std::string name) {
+  auto* player = new Player(world, idCounter, name);
   players.insert(make_pair(name, player));
   player->setPosition(0, 0);
   world.notify(NEW_ENTITY, player);
   idCounter++;
-}
-
-void Game::move(int id, int nextX, int nextY) {
-  Player* player = dynamic_cast<Player *>(world.getEntity(id));
-  player->move(nextX, nextY);
-}
-
-Player *Game::getPlayer(std::string &name) {
-  return players.find(name)->second;
-}
-
-Creature *Game::getCreature(int id) {
-  return dynamic_cast<Creature *>(world.getEntity(id));
 }
 
 void Game::attack(int id, int idEnemy) {
--- server/Game.h
+++ server/Game.h
@@ -12,27 +12,20 @@ class Game {
  public:
   Game();
 
-  void addCharacter(std::string &name);
-  void addCreature();
-
-  void move(int id, int nextX, int nextY);
-  void attack(int id, int idEnemy);
+  void handle(Event& event);
 
   std::string draw();
 
-  bool isRunning() const;
-  void stop();
-
   ~Game();
 
-  // Just for testing
-  Creature *getCreature(int id);
-  Player * getPlayer(std::string &name);
  private:
   int idCounter;
-  bool running;
   World world;
   std::map<std::string, Player *> players;
+
+  void attack(int id, int idEnemy);
+  void addPlayer(std::string name);
+  void addCreature();
 };
 
 #endif
